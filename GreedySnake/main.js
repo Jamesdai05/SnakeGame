7af188e76ec9802616b35349079e55b0523695eb
jamesdai05 <dai_guilong@hotmail.com>
2022-06-27 00:16:55 +0800

    add the movement of the snake

--- GreedySnake/main.js
+++ GreedySnake/main.js
@@ -3,42 +3,87 @@ const ctx = canvas.getContext("2d");
 
 let speed = 5;
 let gridCount = 20;
-let gridSize = canvas.width/gridCount -2;
-let headX = 10;
-let headY = 10;
+let gridSize = canvas.width/gridCount -1;
+let headX = 1;
+let headY = 1;
+
+let foodX = 4;
+let foodY = 5;
+
+let velocityOfX = 0;
+let velocityOfY = 0;
+
 
 
 
 function drawGame() {
     
+    //ClearScreen when the game goes on;
+    cleanScreen();
     drawSnake();
-    clearnScreen();                     //ClearScreen when the game goes on;
+    drawFood();                     
     setTimeout(drawGame, 1000/speed);         // Pending for the game to be reload.
 
 }
-// Refresh the screen window 
-function clearnScreen() {
+// Refresh the screen window to shown the snake movement
+function cleanScreen() {
     ctx.fillStyle ="black";
     ctx.fillRect(0,0,canvas.width,canvas.height);
 };
 // drawSnake
 function drawSnake() {
-    ctx.fillStyle="red";
-    ctx.fillRect(headX*gridCount, headY *gridCount,gridSize,gridSize);
+    ctx.fillStyle ="red";
+    ctx.fillRect(headX * gridCount, headY * gridCount,gridSize,gridSize);
+}
+// snake movement
+document.addEventListener("keydown",directionControl);
+function directionControl(event){
+    //left = 37 right = 39 up = 38 down = 40 (keycode)  
+    switch(event.keyCode){
+        // move left 
+        case 37 :
+          if(velocityOfX ==1){
+            return;
+          } else {
+            velocityOfX =-1;
+            velocityOfY = 0;
+          };
+          break;
+          // move right
+          case 39 :
+          if(velocityOfX ==-1){
+            return;
+          } else {
+            velocityOfX =1;
+            velocityOfY = 0;
+          };
+          break;
+          // move up
+          case 38 :
+          if(velocityOfY == 1){
+            return;
+          } else {
+            velocityOfX =0;
+            velocityOfY =-1;
+          };
+          break;
+          // move down
+          case 40:
+            if(velocityOfY == -1){
+            return;
+          } else {
+            velocityOfX =0;
+            velocityOfY =1;
+          };
+          break;
+    }
 }
 
-
-
-document.addEventListener("keydown",keyDown);
-
-
-// drawFood() {
-//     ctx.fillStyle();
+// function drawFood() {
+//     ctx.fillStyle = "blue";
+//     ctx.fillRect(foodX)
 // }
 
-function keyDown() {
-
-}
 
 
 drawGame();
