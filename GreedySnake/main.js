d06d1ef2a5e48e0c403dfdf528b82a39d22d5034
jamesdai05 <dai_guilong@hotmail.com>
2022-06-26 14:04:37 +0800

    drawSnake added

--- GreedySnake/main.js
+++ GreedySnake/main.js
@@ -2,15 +2,37 @@ const canvas =document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 let speed = 5;
+let gridCount = 25;
+let gridSize = canvas.width/gridCount -2;
+let headX = 12;
+let headY = 12;
+
+
 
 function drawGame() {
     
+    drawSnake();
     clearnScreen();                     //ClearScreen when the game goes on;
     setTimeout(drawGame, 1000);         // Pending for the game to be reload.
 
 }
 
 clearnScreen() {
-    ctx.fillStyle ="rgba(129,215,110,0.8)";
-    ctx.fillStyle(0,0,canvas.canvas.width,canvas.height);
+    ctx.fillStyle ="black";
+    ctx.fillStyle(0,0,canvas.width,canvas.height);
 }
+
+drawSnake() {
+    ctx.fillStyle="yellow";
+    ctx.fillRect(headX*gridCount, headY *gridCount,gridSize,gridSize);
+}
+
+document.addEventListener("keydown",keyDown);
+
+
+drawFood() {
+    ctx.fillStyle();
+}
+
+
+drawGame();
